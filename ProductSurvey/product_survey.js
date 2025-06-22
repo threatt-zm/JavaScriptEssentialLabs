4c5c6aa08b5e4e26caf9809faaa2961641a59972
threatt-zm <10990825@live.mercer.edu>
2025-06-22 01:27:39 -0400

    added textbox for product experience

--- ProductSurvey/product_survey.js
+++ ProductSurvey/product_survey.js
@@ -7,6 +7,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('experienceText').value;
     alert('Thank you for your valuable feedback');
 
     document.getElementById('userName').innerHTML = username;
@@ -16,6 +17,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = experience;
     document.getElementById('userInfo').style.display = 'block';
 }
 
