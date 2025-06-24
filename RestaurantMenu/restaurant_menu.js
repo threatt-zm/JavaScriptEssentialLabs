62b62c73bff53be7c38fac6c4a9d9059e9d560d6
threatt-zm <10990825@live.mercer.edu>
2025-06-24 02:02:37 -0400

    Added menu prices

--- RestaurantMenu/restaurant_menu.js
+++ RestaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $25', 'Pasta - $19.99', 'Burger - $16', 'Salmon - $18'];
+const dessertMenu = ['Cake - $1.99', 'Ice Cream - $3', 'Pudding - $2.99', 'Fruit Salad - $4.99'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
