939f1106c49f2a85987506da6419d22792af1ebd
threatt-zm <10990825@live.mercer.edu>
2025-06-25 19:05:19 -0400

    added Total Length text and reset input functionality

--- SpeedAnalysis/speed_analysis.js
+++ SpeedAnalysis/speed_analysis.js
@@ -4,6 +4,11 @@ let startTime, endTime;
 function startTest() {
     document.getElementById("inputText").value = testText;
 
+    if (document.getElementById("userInput").value !== "") {
+        document.getElementById("userInput").value = "";
+        document.getElementById("userInput").readOnly = false;
+    }
+
     document.getElementById("output").innerHTML = "";
     startTime = new Date().getTime();
 
@@ -19,6 +24,7 @@ function endTest() {
 
     var timeElapsed = (endTime - startTime) / 1000;
     var userTypedText = document.getElementById("userInput").value;
+    var userTypedLength = userTypedText.length;
 
     var typedWords = userTypedText.split(/\s+/).filter(function (word) {
         return word !== "";
@@ -31,7 +37,7 @@ function endTest() {
     }
 
     var outputDiv = document.getElementById("output");
-    outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" + "<p>Words Typed: " + typedWords + "</p>" + "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" + "<p>Words Per Minute (WPM): " + wpm + "</p>";
+    outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" + "<p>Total Length: " + userTypedLength +"</p>" + "<p>Words Typed: " + typedWords + "</p>" + "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" + "<p>Words Per Minute (WPM): " + wpm + "</p>";
 
     var button = document.getElementById("btn");
     button.innerHTML = "Start Test";
