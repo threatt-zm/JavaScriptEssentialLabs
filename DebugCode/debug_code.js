967b3752b4de8a5f1a6acaeffc2699bb2636c597
threatt-zm <10990825@live.mercer.edu>
2025-06-26 17:52:29 -0400

    added more arithmetic operations

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -3,9 +3,15 @@ function performOperation() {
     let num2 = parseInt(document.getElementById('input2').value);
 
     if(!isNaN(num1) && !isNaN(num2)) {
-        let result = multiply(num1, num2);
+        let product = multiply(num1, num2);
+        let quotient = divide(num1, num2);
+        let sum = add(num1, num2);
+        let difference = subtract(num1, num2)
 
-        displayResult(result);
+        displayResult(product, 'product');
+        displayResult(quotient, 'quotient')
+        displayResult(sum, 'sum')
+        displayResult(difference, 'difference')
     } else {
         displayResult('Please enter valid numbers');
     }
@@ -17,7 +23,25 @@ function multiply(a, b) {
     return a * b;
 }
 
-function displayResult(result) {
-    const resultElement = document.getElementById('result');
-    resultElement.textContent = `The result is: ${result}`;
+function divide(a, b) {
+    debugger;
+
+    return a / b;
+}
+
+function add(a, b) {
+    debugger;
+
+    return a + b;
+}
+
+function subtract(a, b) {
+    debugger;
+
+    return a - b;
+}
+
+function displayResult(result, operation) {
+    const resultElement = document.getElementById(operation);
+    resultElement.textContent = `The ${operation} is: ${result}`;
 }
