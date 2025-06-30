b26340aa9ee13e4899cdf0ae71dffe0511976de6
threatt-zm <10990825@live.mercer.edu>
2025-06-30 00:35:20 -0400

    added input for longitude and latitude with functionality

--- WeatherReport/weather_report.js
+++ WeatherReport/weather_report.js
@@ -20,4 +20,28 @@ function showweatherDetails(event) {
     });
 }
 
+function showLongLatDetails(event) {
+    event.preventDefault();
+
+    const lat = document.getElementById('latitude').value;
+    const long = document.getElementById('longitude').value;
+    const apiKey = '78066ce945903904b18bba7869a8e44a';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name} (${long}, ${lat})</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+    });
+}
+
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+document.getElementById('longLatForm').addEventListener('submit', showLongLatDetails);
